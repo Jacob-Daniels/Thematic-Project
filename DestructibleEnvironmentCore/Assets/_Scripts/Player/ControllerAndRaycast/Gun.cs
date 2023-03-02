e7d831aaf5e507766ad337a2d32b2e64c222343a
Link753 <119621532+Link753@users.noreply.github.com>
2023-03-02 12:21:59 +0000

    First person controller + raycast
    Movement is for player object, Lookaround is for camera, gun is for raycast

---
+++ DestructibleEnvironmentCore/Assets/_Scripts/Player/ControllerAndRaycast/Gun.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f, range = 100f, firerate = 15f;
+    public Camera fpsCam;
+    public GameObject bullet;
+
+    private float reload = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time >= reload)
+        {
+            reload = Time.time + 1f / firerate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            if(hit.transform.name == "Door")
+            {
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * 2000);
+            }
+
+            GameObject Bullet = Instantiate(bullet, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(Bullet, 0.5f);
+        }
+    }
+}
