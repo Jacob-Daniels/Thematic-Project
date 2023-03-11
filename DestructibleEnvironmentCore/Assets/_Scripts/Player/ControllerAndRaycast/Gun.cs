0138c4a01526bea5e1ebebb1bf37fbb9a7def0e5
Link753 <119621532+Link753@users.noreply.github.com>
2023-03-11 16:23:29 +0000

    Implemented Pickup

--- DestructibleEnvironmentCore/Assets/_Scripts/Player/ControllerAndRaycast/Gun.cs
+++ DestructibleEnvironmentCore/Assets/_Scripts/Player/ControllerAndRaycast/Gun.cs
@@ -4,38 +4,56 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f, range = 100f, firerate = 15f;
     public Camera fpsCam;
-    public GameObject bullet;
+    //public GameObject bullet;
 
     private float reload = 0f;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Fire1") && Time.time >= reload)
+        if(Input.GetButtonDown("Fire1"))//hold left click
         {
-            reload = Time.time + 1f / firerate;
             Shoot();
+        }
+        else if (Input.GetButtonUp("Fire1"))//release left click
+        {
+            for(int i = 3; i < transform.parent.childCount; i++)
+            {
+                transform.parent.GetChild(i).GetComponent<Collider>().enabled = true;
+                transform.parent.GetChild(i).GetComponent<Transform>().SetParent(null);
+            }
+        }
+        if (Input.GetButtonDown("Fire2"))//hold right click to add to inventory
+        {
+            addtoInventory();
+        }
+        else if(Input.GetButtonUp("Fire2"))//release right click
+        {
+            removefromInventory();
         }
     }
 
     void Shoot()
     {
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))//generate ray
         {
             Debug.Log(hit.transform.name);
-            //Enemy enemy = hit.transform.GetComponent<Enemy>();
-            //if(enemy != null)
-            //{
-            //    enemy.TakeDamage(damage);
-            //}
 
-            if(hit.transform.name == "Door")
+            if(hit.transform.gameObject.layer != 3)//wont pickup anything on layer 3
             {
-                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * 2000);
+                hit.collider.enabled = false;
+                hit.transform.SetParent(transform.parent);
             }
-
-            GameObject Bullet = Instantiate(bullet, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(Bullet, 0.5f);
         }
+    }
+
+    void addtoInventory()
+    {
+        //add to inventory
+    }
+
+    void removefromInventory()
+    {
+        //remove code goes here
     }
 }
