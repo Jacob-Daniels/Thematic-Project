6f025510754049627fc3c787ae4793716a66d205
Cnmhqwerty <Cnmh982@gmail.com>
2023-03-26 15:32:25 +0100

    Scale Broken Pieces by Distance

--- DestructibleEnvironmentCore/Assets/_Scripts/Tools & Weapons/Destroy.cs
+++ DestructibleEnvironmentCore/Assets/_Scripts/Tools & Weapons/Destroy.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+    private float dist;
+    [SerializeField] GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
     private void Update()
     {
-        if (transform.localScale.x <= 0)
+        dist = Mathf.Clamp(Vector3.Distance(transform.position, player.transform.position)-2f,0,1);
+        Debug.Log(dist);
+        transform.localScale = new Vector3(dist,dist,dist);
+        if (dist <= 0.1)
         {
             // Destroy parent if object is its last child
-            if (transform.parent.childCount == 1)
+            if (transform.parent.childCount <= 1)
             {
                 Destroy(transform.parent.gameObject);
             }
